ade15df6a114f095a1dff0b5e5b632de1c665963
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-08-12 16:44:45 +0530

    Working with Replit and new demands by @CreativeGit

--- Source/Bot.ts
+++
@@ -1,6 +0,0 @@
-import { config } from "dotenv";
-import StaffListerClient from "./Base/Client";
-config();
-
-new StaffListerClient()
-	.start();
--- Source/Util/LoadJSON.ts
+++
@@ -1,7 +0,0 @@
-import { readFile } from "fs/promises";
-
-export default async function loadJSON(path: string) {
-	const data = (await readFile(path)).toString();
-
-	return JSON.parse(data);
-}
