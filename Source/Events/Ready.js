8e87dc4e982aa5cc21661bd1cf335bb979d78b1a
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-08-16 23:39:30 +0530

    :astonished: Added the activity stuff

--- Source/Events/Ready.js
+++ Source/Events/Ready.js
@@ -6,9 +6,15 @@ module.exports.name = "ready";
 /**
  * @param {import("discord.js").Client} client
  */
-module.exports.run = function(client) {
+module.exports.run = async function(client) {
 	client.logger.success("client", `Logged in as ${client.user.tag}`);
 
 	client.guilds.cache.get(process.env.GUILD_ID).commands.set(client.commands.map(c => c.config));
 	client.logger.success("client/commands", `Loaded ${client.commands.size} commands in guild of ID: ${process.env.GUILD_ID}`);
+
+	client.user.setActivity({
+		name: "RJain",
+		type: "STREAMING",
+		url: "https://www.youtube.com/watch?v=YrgQxLoZHdQ",
+	});
 };
